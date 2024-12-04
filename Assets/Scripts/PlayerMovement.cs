e697cadae38f9f482cc3422ecbe6dc9f374b318c
MasonTheBag <masonthebag@gmail.com>
2024-12-04 08:31:13 -0500

    some more stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private float speed = 10;
+    private Vector3 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Handle left and right movement (A and D)
+        if (Input.GetKey(KeyCode.A)) // Move left
+        {
+            movement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D)) // Move right
+        {
+            movement += Vector3.right;
+        }
+
+        // Normalize movement to prevent faster diagonal movement
+        movement = movement.normalized * speed * Time.deltaTime;
+
+
     }
 }
